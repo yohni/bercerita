726b090232e7a54e638ef9f2c38d61f4afee18c3
yohni <yohni.123@gmail.com>
2020-02-07 00:00:08 +0700

    Implement react router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './styles/index.scss';
 import Main from './containers';
+import Navbar from './components/navbar';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Main />, document.getElementById('root'));
+const App = () =>(
+  <div className='main-container'>
+    <Navbar />
+    <div className='main-content'>
+      <Route exact={true} path='/' component={Main} />
+    </div>
+  </div>
+)
+
+ReactDOM.render(
+  <Router>
+    <App/>
+  </Router>, 
+  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
