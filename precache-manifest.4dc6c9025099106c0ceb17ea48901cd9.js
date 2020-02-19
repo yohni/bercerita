a694fd37be29adc594e9d3fee7cea485319ae60a
yohni <yohni.123@gmail.com>
2020-02-19 15:02:33 +0700

    Updates

---
+++ precache-manifest.4dc6c9025099106c0ceb17ea48901cd9.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd97f5a9d6f122793b95252b25bf439d",
+    "url": "/bercerita/index.html"
+  },
+  {
+    "revision": "e8056f7c6501b197bf14",
+    "url": "/bercerita/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "aac6de92151869698ba0",
+    "url": "/bercerita/static/css/main.cd45900d.chunk.css"
+  },
+  {
+    "revision": "e8056f7c6501b197bf14",
+    "url": "/bercerita/static/js/2.8a28bb8c.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/bercerita/static/js/2.8a28bb8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aac6de92151869698ba0",
+    "url": "/bercerita/static/js/main.0fe3c8fc.chunk.js"
+  },
+  {
+    "revision": "35abded54050d788f8ca",
+    "url": "/bercerita/static/js/runtime-main.6c115bba.js"
+  },
+  {
+    "revision": "8ba765ce9580a3bdae76b59130d0ef8a",
+    "url": "/bercerita/static/media/AmaticSC-Bold.8ba765ce.ttf"
+  },
+  {
+    "revision": "e9a42e339f93a719c17be53c386dbbfb",
+    "url": "/bercerita/static/media/AmaticSC-Regular.e9a42e33.ttf"
+  },
+  {
+    "revision": "1dce24d6b0c51719257d20c82a1f6346",
+    "url": "/bercerita/static/media/NatureMan.1dce24d6.png"
+  },
+  {
+    "revision": "2d0bdc8df10dee036ca3bedf6f3647c6",
+    "url": "/bercerita/static/media/OpenSans-Light.2d0bdc8d.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/bercerita/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "a7b29c14a457047c6dc634b58b5b1517",
+    "url": "/bercerita/static/media/WorkingTable.a7b29c14.png"
+  },
+  {
+    "revision": "c7c27c56cf336c4298ea3cf02590edf8",
+    "url": "/bercerita/static/media/project1.c7c27c56.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bc2134d263538ae24f59991845dbbce1.js"
+  "/bercerita/precache-manifest.4dc6c9025099106c0ceb17ea48901cd9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bercerita/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
