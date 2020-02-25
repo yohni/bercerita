0d7287b02f57e72e70ecfc70ab94cdc6a92561ec
yohni <yohni.123@gmail.com>
2020-02-25 10:46:52 +0700

    Implement react router link

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,41 +1,52 @@
-import React, { Component } from 'react';
-import Logo from '../logo';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+import Logo from "../logo";
 
 class Navbar extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-      menuOpen: false,
-    }
+    this.state = {
+      menuOpen: false
+    };
   }
 
   handleMenuClick = e => {
     e.preventDefault();
-    this.setState({menuOpen: !this.state.menuOpen});
-    console.log(this.state.menuOpen)
-  }
-    
+    this.setState({ menuOpen: !this.state.menuOpen });
+    console.log(this.state.menuOpen);
+  };
 
-  handleLinkClick(){
-    this.setState({menuOpen: false});
+  handleLinkClick() {
+    this.setState({ menuOpen: false });
   }
 
   render() {
     return (
-      <div className='nav'>
-        <div className='nav-logo'>
-          <Logo />
+      <div className="nav">
+        <div className="nav-logo">
+          <Link to="/">
+            <Logo />
+          </Link>
         </div>
         <div className="nav-list">
-          <div className='hamburger' onClick={this.handleMenuClick}>
-            <span className={`${this.state.menuOpen ? 'lineTop' : ''}`}></span>
-            <span className={`${this.state.menuOpen ? 'lineMid' : ''}`}></span>
-            <span className={`${this.state.menuOpen ? 'lineBot' : ''}`}></span>
+          <div className="hamburger" onClick={this.handleMenuClick}>
+            <span className={`${this.state.menuOpen ? "lineTop" : ""}`}></span>
+            <span className={`${this.state.menuOpen ? "lineMid" : ""}`}></span>
+            <span className={`${this.state.menuOpen ? "lineBot" : ""}`}></span>
           </div>
-          <ul className={`collapse ${this.state.menuOpen ? 'is-expanded' : ''}`}>
-            <li className='navlink'><a href='#meId'>Tentang Aku</a></li>
-            <li className='navlink'><a href='#expId'>Corat-Coret</a></li>
-            <li className='navlink'><a href='#contactId'>Kontak</a></li>
+          <ul
+            className={`collapse ${this.state.menuOpen ? "is-expanded" : ""}`}
+          >
+            <li className="navlink">
+              <a href="#meId">Tentang Aku</a>
+            </li>
+            <li className="navlink">
+              <a href="#expId">Corat-Coret</a>
+            </li>
+            <li className="navlink">
+              <a href="#contactId">Kontak</a>
+            </li>
           </ul>
         </div>
       </div>
